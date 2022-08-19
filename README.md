eebc7977cf3a6392a18d37a3a46ee19b1e32c5e5
obinna aguwa <macmartins081@gmail.com>
2022-08-19 19:24:59 +0100

    minted nft

---
+++ README.md
@@ -0,0 +1,19 @@
+# NFT SmartContract
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+This project demonstrate an NFT smart contract that allows only the smart contract creator to mint any amount of NFT and it was uploaded to OpenSea.
+
+A sample NFT was deployed. 
+
+Here is the URL address to the NFT on open sea: 
+https://testnets.opensea.io/collection/mynft-ytendq4pwc
+
+To run the code, do running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+GAS_REPORT=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,24 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT")
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy()
+  await myNFT.deployed()
+  console.log("Contract deployed to address:", myNFT.address)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+  // 0xC366351A84a16edd26E183F7Bc5bD012FC148B3c
